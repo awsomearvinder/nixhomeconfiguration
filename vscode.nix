5726c0445e4ed54cec9325018dff2c15fac290d9
awesomearvinder <ArvinderDhan@gmail.com>
2020-09-07 09:48:46 -0500

    add rust-analyzer extension

--- vscode.nix
+++ vscode.nix
@@ -5,7 +5,12 @@ let
     #matklad.rust-analyzer
     ms-vscode.cpptools
     vscodevim.vim
-  ];
+  ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [{
+    name="rust-analyzer";
+    publisher="matklad";
+    version="0.2.297";
+    sha256 = "0pj29k5pm1p7f987x9rjd0pks552fxvjv72dscxsk84svl132s0f";
+  }];
   vscode = pkgs.vscode-with-extensions.override {
     vscodeExtensions = extensions;
   };
