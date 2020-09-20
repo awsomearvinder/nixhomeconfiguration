a3179655505d32ced74ac9ffa2ee305e18753e63
awesomearvinder <ArvinderDhan@gmail.com>
2020-09-20 17:30:47 -0500

    initial sway support

--- home.nix
+++ home.nix
@@ -1,4 +1,6 @@
-{ config, pkgs, ... } : {
+{ config, lib, pkgs, ... } : 
+with lib;
+{
   imports = [
     ./firefox.nix
     ./alacritty.nix
@@ -10,30 +12,56 @@
     ./spotify.nix
     ./xournal.nix
     ./pulseeffects.nix
+    ./mako.nix
   ];
 
-  programs.neovim = import ./neovim.nix pkgs;
 
-  # Let Home Manager install and manage itself.
-  programs.home-manager.enable = true;
+  options = {
+    dots = mkOption {
+      type = types.path;
+    };
+    modifier = mkOption {
+      type = types.str;
+    };
+    scripts = mkOption {
+      type = types.path;
+    };
+  };
 
-  # Home Manager needs a bit of information about you and the
-  # paths it should manage.
-  home.username = builtins.getEnv "USER";
-  home.homeDirectory = builtins.getEnv "HOME";
+  config = {
+    nixpkgs.config = {
+      allowUnfree=true;
+    };
 
-  home.packages = with pkgs; [
-    fzf # this is required for nvim. not detected as a dep but /shrug
-    ytop # top sucks.
-  ];
+    dots = ./dotfiles;
+    modifier = "Mod4";
+    scripts = /home/bender/.config/scripts;
 
-  # This value determines the Home Manager release that your
-  # configuration is compatible with. This helps avoid breakage
-  # when a new Home Manager release introduces backwards
-  # incompatible changes.
-  #
-  # You can update Home Manager without changing this value. See
-  # the Home Manager release notes for a list of state version
-  # changes in each release.
-  home.stateVersion = "20.09";
+    # Let Home Manager install and manage itself.
+    programs.home-manager.enable = true;
+
+    #This is kinda gross, I know.
+    programs.neovim = import ./neovim.nix pkgs;
+
+    # Home Manager needs a bit of information about you and the
+    # paths it should manage.
+    home.username = builtins.getEnv "USER";
+    home.homeDirectory = builtins.getEnv "HOME";
+
+    home.packages = with pkgs; [
+      fzf # this is required for nvim's coc-fzf. not detected as a dep but /shrug
+      ytop # top sucks.
+      mako #notification daemon.
+    ];
+
+    # This value determines the Home Manager release that your
+    # configuration is compatible with. This helps avoid breakage
+    # when a new Home Manager release introduces backwards
+    # incompatible changes.
+    #
+    # You can update Home Manager without changing this value. See
+    # the Home Manager release notes for a list of state version
+    # changes in each release.
+    home.stateVersion = "20.09";
+  };
 }
