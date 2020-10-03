3963624d5e2bddbd8f11786e903361098edd7a3c
awesomearvinder <ArvinderDhan@gmail.com>
2020-10-03 18:42:03 -0500

    run nixfmt on discord overlay

--- overlays/discordOverlay.nix
+++ overlays/discordOverlay.nix
@@ -1,3 +1,6 @@
 self: super: {
-  discord = super.discord.overrideAttrs (_: { src = builtins.fetchTarball "https://discordapp.com/api/download?platform=linux&format=tar.gz"; });
+  discord = super.discord.overrideAttrs (_: {
+    src = builtins.fetchTarball
+      "https://discordapp.com/api/download?platform=linux&format=tar.gz";
+  });
 }
