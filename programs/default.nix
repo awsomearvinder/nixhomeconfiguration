203b7da54df03c952ce9c413a8a945b5302ab56b
awesomearvinder <ArvinderDhan@gmail.com>
2021-06-06 18:45:06 -0500

    remove unused programs folder

--- programs/default.nix
+++
@@ -1,8 +0,0 @@
-{ config, pkgs, ... }: {
-  enviorment = {
-    systemPackages = [ sway ];
-  }
-
-  #Make available to display manager if exists.
-  services.xserver.displayManager.sessionPackages = [ sway ];
-}
