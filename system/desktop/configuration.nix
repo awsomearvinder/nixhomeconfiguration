3f8ffbb69136b1385e06d0b2d50647f978a4cb71
awesomearvinder <ArvinderDhan@gmail.com>
2021-06-11 19:15:11 -0500

    Change desktopManager.gnome3 to desktopManager.gnome
    services.xserver.desktopManager.gnome3.enable ->
    services.xserver.desktopManager.gnome.enable
    
    The former is a deprecated name.

--- system/desktop/configuration.nix
+++ system/desktop/configuration.nix
@@ -52,7 +52,7 @@
   # Enable the GNOME 3 Desktop Environment.
   services.xserver.enable = true;
   services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome3.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
 
   # Configure keymap in X11
   # services.xserver.layout = "us";
--- system/laptop/configuration.nix
+++ system/laptop/configuration.nix
@@ -70,7 +70,7 @@
   # Enable the X11 windowing system.
   services.xserver.enable = true;
   services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome3.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
   services.xserver.layout = "us";
 
   #printing
