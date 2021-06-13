907e5a309e26793519b12def5b01445567eda338
awesomearvinder <ArvinderDhan@gmail.com>
2021-06-13 04:55:28 -0500

    Move away from settings.nix in favor of flake output.
    The reason for the move away is that even though now you have to specify
    the system you're on in the CLI - you no longer have to have a
    settings.nix which you have to keep updated with what system you're on
    while trying to keep it away from git and flake's weird interactions
    with git.

--- flake.nix
+++ flake.nix
@@ -4,29 +4,31 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager";
+    settings.url = "path:./settings.nix";
+    settings.flake = false;
   };
 
-  outputs = { home-manager, nixpkgs, ... }:
-    let settings = import ./settings.nix;
+  outputs = { home-manager, nixpkgs, settings, ... }:
+    let
+      baseModules = [
+        (home-manager.nixosModules.home-manager)
+        {
+          config = {
+            home-manager.users.bender = import ./bender/home.nix;
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            nixpkgs.overlays = import ./bender/overlays;
+          };
+        }
+      ];
     in {
-      nixosConfigurations.nixos = (nixpkgs.lib.nixosSystem {
+      nixosConfigurations.desktop = (nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
-        modules = ([
-          (home-manager.nixosModules.home-manager)
-          {
-            config = {
-              home-manager.users.bender = import ./bender/home.nix;
-              home-manager.useGlobalPkgs = true;
-              home-manager.useUserPackages = true;
-              nixpkgs.overlays = import ./bender/overlays;
-            };
-          }
-        ] ++ (if settings.machine == "laptop" then
-          [ ./system/laptop/configuration.nix ]
-        else if settings.machine == "desktop" then
-          [ ./system/desktop/configuration.nix ]
-        else
-          throw "Please pass in a machine to settings.nix"));
+        modules = baseModules ++ [ ./system/desktop/configuration.nix ];
+      });
+      nixosConfigurations.laptop = (nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = baseModules ++ [ ./system/laptop/configuration.nix ];
       });
     };
 }
