0f3d9bcd6b041b0b52c44ec69141678e4c596362
awesomearvinder <ArvinderDhan@gmail.com>
2021-07-16 05:50:04 -0500

    Use overlay in discordOverlay.nix again (instead of hardcoding).

--- bender/overlays/default.nix
+++ bender/overlays/default.nix
@@ -1,11 +1,4 @@
 [
   (import ./neovimOverlay.nix)
   (import ./discordOverlay.nix)
-  #(self: super:
-  #  {
-  #    discord = super.discord.overrideAttrs (_: {
-  #      src = builtins.fetchTarball {
-  #        src = "https://discordapp.com/api/download?platform=linux&format=tar.gz";
-  #    }});
-  #  })
 ]
--- bender/overlays/discordOverlay.nix
+++ bender/overlays/discordOverlay.nix
@@ -1,7 +1,8 @@
-(self: super:
-  {
-    #discord = super.discord.overrideAttrs (_: {
-    #  src = builtins.fetchTarball {
-    #    src = "https://discordapp.com/api/download?platform=linux&format=tar.gz";
-    #}});
-  })
+(self: super: {
+  discord = super.discord.overrideAttrs (_: {
+    src = builtins.fetchTarball {
+      url = "https://discordapp.com/api/download?platform=linux&format=tar.gz";
+      sha256 = "1ahj4bhdfd58jcqh54qcgafljqxl1747fqqwxhknqlasa83li75n";
+    };
+  });  
+})
