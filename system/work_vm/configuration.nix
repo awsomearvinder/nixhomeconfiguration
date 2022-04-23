4a04e7807f899eb49d09e5d78008e1fd3cbc1dcf
Arvinder <ArvinderDhan@gmail.com>
2022-04-23 21:59:48 +0000

    Add pinentry

--- system/work_vm/configuration.nix
+++ system/work_vm/configuration.nix
@@ -97,6 +97,7 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
+    pinentry
   ];
 
   environment.variables.EDITOR = "nvim";
