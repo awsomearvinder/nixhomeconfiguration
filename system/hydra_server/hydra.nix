5b856e96e76e11f287acd3e007562801b7342b11
Arvinder <ArvinderDhan@gmail.com>
2022-05-03 08:33:44 -0500

    Add pcparadise org to hercules-ci

--- system/hydra_server/hydra.nix
+++ system/hydra_server/hydra.nix
@@ -1,11 +1,13 @@
 { pkgs, config, lib, ... }:
 {
   age.secrets."hydra/cluster-join-token.key".file = ../../secrets/hydra/cluster-join-token.key;
-  age.secrets."hydra/cluster-join-token.key".owner = "hercules-ci-agent";
+  age.secrets."hydra/cluster-join-token.key".owner = "hci-personal";
   age.secrets."hydra/binary-caches.json".file = ../../secrets/hydra/binary-caches.json;
-  age.secrets."hydra/binary-caches.json".owner = "hercules-ci-agent";
-  age.secrets."hydra/pcparadise-cluster-join-token.key".file = ../../secrets/hydra/cluster-join-token.key;
-  age.secrets."hydra/pcparadise-cluster-join-token.key".owner = "hercules-ci-agent";
+  age.secrets."hydra/binary-caches.json".owner = "hci-personal";
+  age.secrets."hydra/pcparadise-cluster-join-token.key".file = ../../secrets/hydra/pcparadise-cluster-join-token.key;
+  age.secrets."hydra/pcparadise-cluster-join-token.key".owner = "hci-pcparadisebot";
+  age.secrets."hydra/pcparadise-binary-caches.json".file = ../../secrets/hydra/binary-caches.json;
+  age.secrets."hydra/pcparadise-binary-caches.json".owner = "hci-pcparadisebot";
   services.hercules-ci-agents = {
     personal = {
       settings = {
@@ -17,7 +19,7 @@
     pcparadisebot = {
       settings = {
         clusterJoinTokenPath = config.age.secrets."hydra/pcparadise-cluster-join-token.key".path;
-        binaryCachesPath = config.age.secrets."hydra/binary-caches.json".path;
+        binaryCachesPath = config.age.secrets."hydra/pcparadise-binary-caches.json".path;
         concurrentTasks = 4;
       };
     };
