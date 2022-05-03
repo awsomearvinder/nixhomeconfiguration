5917e8aeed84d703382960fa6177612981d37ebe
Arvinder <ArvinderDhan@gmail.com>
2022-05-03 08:31:03 -0500

    Enable rerere by default

--- bender/applications/git.nix
+++ bender/applications/git.nix
@@ -8,6 +8,7 @@
       user.signingkey = "687341EC5B73B98BC5E4DC5D4599D30196519D5F";
       commit.gpgSign = true;
       gpg.program = "${pkgs.gnupg}/bin/gpg";
+      rerere.enabled = true;
     };
   };
 }
