39785571e6eb5fb5ff3160de8272fd7b09649e9a
Arvinder <ArvinderDhan@gmail.com>
2022-05-07 13:45:39 -0500

    Add hls

--- bender/dotfiles/nvim/lua/config/lsp.lua
+++ bender/dotfiles/nvim/lua/config/lsp.lua
@@ -29,7 +29,7 @@ cmp.setup({
 })
 
 local nvim_lsp = require'lspconfig';
-local servers = {"rust_analyzer", "pyright", "tsserver", "elmls"};
+local servers = {"rust_analyzer", "pyright", "tsserver", "elmls", "hls"};
 for _, server in ipairs(servers) do 
   nvim_lsp[server].setup {
     capabilities = require('cmp_nvim_lsp')
