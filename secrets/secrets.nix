ebea050f37b3ef4de63d04134d55e68275d38a02
Arvinder <ArvinderDhan@gmail.com>
2022-05-12 14:20:51 -0500

    coding_server -> wireguard_server

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,11 +1,11 @@
 let 
-  coding_server = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIuYCV7NwqlACA5rp8WH4lerKB8M9Ci3jucyW0hhQLfD"; 
+  wireguard_server = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIuYCV7NwqlACA5rp8WH4lerKB8M9Ci3jucyW0hhQLfD"; 
   desktop = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINEsyr9uX4OFH3vsoiIxeW5D9ZEam2tbg/mQRZ6Ov2aL"; 
   hydra = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJOGdxM+MWYh+w57KX6Jhby2eK5VEyua65iaxTSCsunv"; 
   nas = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID1VIpEZ+5+4Os3WQ5X8g+65uw/X12EMFQJtKq6Jsg4s root@nixos";
 in {
-  "wireguard/private.key".publicKeys = [coding_server];
-  "dyndns/password.priv".publicKeys = [coding_server];
+  "wireguard/private.key".publicKeys = [wireguard_server];
+  "dyndns/password.priv".publicKeys = [wireguard_server];
   "desktop/wireguard/private.key".publicKeys = [desktop];
   "hydra/wireguard/private.key".publicKeys = [hydra];
   "hydra/cluster-join-token.key".publicKeys = [hydra];
