de57265e6eef5ebb22fad87a94eaa5dd95b20a5b
Arvinder <ArvinderDhan@gmail.com>
2022-05-13 13:18:07 -0500

    Update tmux prefix.

--- bender/applications/tmux.nix
+++ bender/applications/tmux.nix
@@ -9,6 +9,7 @@
       disableConfirmationPrompt = true;
       terminal = "screen-256color";
       shell = "${pkgs.ion}/bin/ion";
+      prefix = "C-a";
       keyMode = "vi";
       reverseSplit = true;
       extraConfig = builtins.readFile (config.dots + "/tmux.conf");
