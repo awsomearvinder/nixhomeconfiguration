c9a696a6dc3825ae8a6f8a4d47133bec9fc54c41
Arvinder <ArvinderDhan@gmail.com>
2022-05-17 13:22:59 -0500

    new keybindings for switching neovim panes

--- bender/dotfiles/nvim/lua/config/keybindings.lua
+++ bender/dotfiles/nvim/lua/config/keybindings.lua
@@ -15,10 +15,10 @@ wk.register({
 }, {prefix = "<leader>"})
 
 -- Navigating windows
-vim.api.nvim_set_keymap('n', '<A-h>', ':wincmd h<CR>', {noremap = true;});
 vim.api.nvim_set_keymap('i', '<C-c>', '<ESC>', {noremap = true;});
-vim.api.nvim_set_keymap('n', '<A-j>', ':wincmd j<CR>', {noremap = true;});
-vim.api.nvim_set_keymap('n', '<A-k>', ':wincmd k<CR>', {noremap = true;});
-vim.api.nvim_set_keymap('n', '<A-l>', ':wincmd l<CR>', {noremap = true;});
+vim.api.nvim_set_keymap('n', 'gh', ':wincmd h<CR>', {noremap = true;});
+vim.api.nvim_set_keymap('n', 'gj', ':wincmd j<CR>', {noremap = true;});
+vim.api.nvim_set_keymap('n', 'gk', ':wincmd k<CR>', {noremap = true;});
+vim.api.nvim_set_keymap('n', 'gl', ':wincmd l<CR>', {noremap = true;});
 vim.api.nvim_set_keymap('n', '<leader>d', ":put =strftime('- [%a %Y-%m-%d %H:%M:%S%z]')<CR>f]a", {noremap = true;})
 vim.api.nvim_set_keymap('t', '<ESC>', '<C-\\><C-n>', {noremap = true;});
