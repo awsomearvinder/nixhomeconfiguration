83eeb0dd1d98a3013825d1ae2b3e5b2bc7b9ab60
Arvinder <ArvinderDhan@gmail.com>
2022-06-22 12:27:55 -0500

    Switch neovim to derivation.

--- bender/applications/neovim.nix
+++ bender/applications/neovim.nix
@@ -9,50 +9,54 @@ in let
   configFiles = builtins.readDir (dots + "/nvim");
   configFileNames =
     pkgs.lib.attrsets.mapAttrsToList (key: value: key) configFiles;
-in {
-  home.packages = with pkgs; [
-    bat # required by my nixconfig
-    fzf # required by my nixconfig
-  ];
-  xdg.configFile = builtins.listToAttrs (builtins.map (name: {
-      name = "nvim/" + name;
-      value = {source = "${dots}/nvim/${name}";};
-    })
-    configFileNames);
-  programs.neovim = let
-    startupPlugins = with pkgs.vimPlugins; [
-      auto-pairs
-      vim-highlightedyank
-      vim-rooter
-      gruvbox-nvim
-      yats-vim
-      vim-gitgutter
-      vimtex
-      vim-sensible
-      vim-nix
-      (nvim-treesitter.withPlugins (p: builtins.attrValues p))
-      nvim-lspconfig
-      telescope-nvim
-      pkgs.nixpkgs-master.vimPlugins.telescope-ui-select-nvim
-      nerdtree
-      neoformat
-      orgmode
-      luasnip
-      pkgs.nixpkgs-master.vimPlugins.which-key-nvim
-      nvim-cmp
-      cmp-nvim-lsp
-      cmp-buffer
-    ];
-  in {
-    enable = true;
+  configured-neovim = pkgs.wrapNeovim pkgs.neovim-unwrapped {
     vimAlias = true;
     viAlias = true;
     withNodeJs = true;
-    package = pkgs.nixpkgs-unstable.neovim-unwrapped;
-    withRuby = true;
-    extraConfig = "lua require('config')";
-    plugins =
-      []
-      ++ (builtins.map (plugin: {inherit plugin;}) startupPlugins);
+    withRuby = false;
+    withPython3 = true;
+    configure = {
+      customRC = "lua require('config')";
+      packages.myVimPackage.start = with pkgs.vimPlugins; [
+        auto-pairs
+        vim-highlightedyank
+        vim-rooter
+        gruvbox-nvim
+        yats-vim
+        vim-gitgutter
+        vimtex
+        vim-sensible
+        vim-nix
+        (nvim-treesitter.withPlugins (p: builtins.attrValues p))
+        nvim-lspconfig
+        telescope-nvim
+        pkgs.nixpkgs-master.vimPlugins.telescope-ui-select-nvim
+        nerdtree
+        neoformat
+        orgmode
+        luasnip
+        pkgs.nixpkgs-master.vimPlugins.which-key-nvim
+        nvim-cmp
+        cmp-nvim-lsp
+        cmp-buffer
+      ];
+    };
   };
+  custom-neovim = pkgs.stdenv.mkDerivation {
+    name = "custom-neovim";
+    unpackPhase = "true";
+    buildInputs = [configured-neovim pkgs.makeWrapper];
+    buildPhase = "";
+    installPhase = ''
+      mkdir -p $out/bin
+      ln -s ${configured-neovim}/bin/nvim $out/bin/nvim
+      wrapProgram $out/bin/nvim --set XDG_CONFIG_HOME ${dots} \
+        --prefix PATH ${pkgs.lib.makeBinPath [pkgs.fzf pkgs.bat]}
+    '';
+  };
+
+in {
+  home.packages = with pkgs; [
+    custom-neovim
+  ];
 }
