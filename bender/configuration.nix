f59fb53c61e058942ccfc32404d2a3b391fbdf57
Arvinder <Arvinder.Dhanoa@winona.edu>
2022-07-07 13:58:27 -0500

    config: change modifier

--- bender/configuration.nix
+++ bender/configuration.nix
@@ -1,5 +1,5 @@
 {work_account, ...}: {
   dots = ./dotfiles;
   scripts = ~/.config/scripts;
-  modifier = if !work_account then "Mod4" else "Control";
+  modifier = if !work_account then "Mod4" else "Mod1";
 }
