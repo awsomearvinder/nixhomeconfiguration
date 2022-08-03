a89c30a41e36fe1b612253cfda48325e743d8368
Arvinder <Arvinder.Dhanoa@winona.edu>
2022-08-03 12:30:21 -0500

    bug: fix neovim post flake update
    This commit moved to manually wrapping neovim.

--- bender/applications/neovim.nix
+++ bender/applications/neovim.nix
@@ -6,6 +6,36 @@
   inherit (config) dots;
 in let
   buildVimPlugin = pkgs.vimUtils.buildVimPlugin;
+  plugins = with pkgs.vimPlugins; [
+    auto-pairs
+    vim-highlightedyank
+    vim-rooter
+    gruvbox-nvim
+    yats-vim
+    vim-gitgutter
+    vimtex
+    vim-sensible
+    vim-nix
+    (nvim-treesitter.withPlugins (p: builtins.attrValues p))
+    nvim-lspconfig
+    telescope-nvim
+    pkgs.nixpkgs-master.vimPlugins.telescope-ui-select-nvim
+    nerdtree
+    neoformat
+    orgmode
+    luasnip
+    pkgs.nixpkgs-master.vimPlugins.which-key-nvim
+    nvim-cmp
+    cmp-nvim-lsp
+    cmp-buffer
+  ];
+  plugins-folder = pkgs.stdenv.mkDerivation {
+    name = "hugos-neovim-plugins";
+    buildCommand = ''
+      mkdir -p $out/nvim/site/pack/plugins/start/
+      ${pkgs.lib.concatMapStringsSep "\n" (path: "ln -s ${path} $out/nvim/site/pack/plugins/start/")  plugins }
+      '';
+  };
   configured-neovim = pkgs.wrapNeovim pkgs.neovim-unwrapped {
     vimAlias = true;
     viAlias = true;
@@ -46,8 +76,9 @@ in let
     buildPhase = "";
     installPhase = ''
       mkdir -p $out/bin
-      ln -s ${configured-neovim}/bin/nvim $out/bin/nvim
+      ln -s ${pkgs.neovim}/bin/nvim $out/bin/nvim
       wrapProgram $out/bin/nvim --set XDG_CONFIG_HOME ${dots} \
+        --set XDG_DATA_DIRS ${plugins-folder} \
         --prefix PATH ${pkgs.lib.makeBinPath [pkgs.fzf pkgs.bat]}
     '';
   };
