1a7710223643289f175a8cab54b845aad9924606
Arvinder <ArvinderDhan@gmail.com>
2022-08-04 00:28:35 -0500

    chore: remove unused code in neovim

--- bender/applications/neovim.nix
+++ bender/applications/neovim.nix
@@ -37,39 +37,6 @@ in let
       ${pkgs.lib.concatMapStringsSep "\n" (path: "ln -s ${path} $out/nvim/site/pack/plugins/start/")  plugins }
       '';
   };
-  configured-neovim = pkgs.wrapNeovim pkgs.neovim-unwrapped {
-    vimAlias = true;
-    viAlias = true;
-    withNodeJs = true;
-    withRuby = false;
-    withPython3 = true;
-    configure = {
-      customRC = "lua require('config')";
-      packages.myVimPackage.start = with pkgs.vimPlugins; [
-        auto-pairs
-        vim-highlightedyank
-        vim-rooter
-        gruvbox-nvim
-        yats-vim
-        vim-gitgutter
-        vimtex
-        vim-sensible
-        vim-nix
-        (nvim-treesitter.withPlugins (p: builtins.attrValues p))
-        nvim-lspconfig
-        telescope-nvim
-        pkgs.nixpkgs-master.vimPlugins.telescope-ui-select-nvim
-        nerdtree
-        neoformat
-        orgmode
-        luasnip
-        pkgs.nixpkgs-master.vimPlugins.which-key-nvim
-        nvim-cmp
-        cmp-nvim-lsp
-        cmp-buffer
-      ];
-    };
-  };
   custom-neovim = pkgs.stdenv.mkDerivation {
     name = "custom-neovim";
     unpackPhase = "true";
