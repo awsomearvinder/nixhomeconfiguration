41dde824d684c4713a886acc466e9d43d1cc316b
Arvinder <ArvinderDhan@gmail.com>
2022-08-07 22:05:21 -0500

    feat: remove postgres

--- system/desktop/configuration.nix
+++ system/desktop/configuration.nix
@@ -168,17 +168,6 @@
   # started in user sessions.
   programs.mtr.enable = true;
 
-  # some development thing I made on the spot.
-  services.postgresql = {
-    enable = true;
-    package = pkgs.postgresql_10;
-    enableTCPIP = true;
-    authentication = pkgs.lib.mkOverride 10 ''
-      local all all trust
-      host all all ::1/128 trust
-      host all all 127.0.0.1/32 trust
-    '';
-  };
 
   # List services that you want to enable:
 
