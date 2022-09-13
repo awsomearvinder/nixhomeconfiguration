321cd0bd738432ad0e2d54343ac7ecaadbcb0969
Arvinder <ArvinderDhan@gmail.com>
2022-09-13 14:45:11 -0500

    feat: add difftastic for git

--- bender/applications/git.nix
+++ bender/applications/git.nix
@@ -3,6 +3,8 @@
     enable = true;
     userName = "Arvinder";
     userEmail = "ArvinderDhan@gmail.com";
+    difftastic.enable = true;
+    difftastic.background = "dark";
     extraConfig = {
       pull.ff = "only";
       user.signingkey = "687341EC5B73B98BC5E4DC5D4599D30196519D5F";
