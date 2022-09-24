e2d8557935b70d90f45fffcfca880ce647401193
Arvinder <ArvinderDhan@gmail.com>
2022-09-24 14:55:31 -0500

    feat: we have sliders for audio levels now!

--- bender/applications/eww.nix
+++ bender/applications/eww.nix
@@ -1,5 +1,10 @@
 { config, pkgs, ... }:
 {
+  programs.eww = {
+    enable = true;
+    package = pkgs.eww-wayland;
+    configDir = config.dots + "/eww_bar";
+  };
   home.packages = [
     pkgs.eww-wayland
     pkgs.jq
