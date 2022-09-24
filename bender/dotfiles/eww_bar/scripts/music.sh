99d003aff3a2e2b97cf3e93b030599f424c5ab0a
Arvinder <ArvinderDhan@gmail.com>
2022-09-24 13:50:11 -0500

    feat: add cover art!

--- bender/dotfiles/eww_bar/scripts/music.sh
+++ bender/dotfiles/eww_bar/scripts/music.sh
@@ -1,8 +1,23 @@
 #!/usr/bin/env bash
+PLAYER="tidal-hifi"
 case $1 in
     "current-song")
         while true; do
             sleep 0.05
-            playerctl metadata title
+            playerctl -p $PLAYER metadata title
         done
+        ;;
+    "image")
+        temp_dir=$(mktemp -d)
+        current_song=""
+        while true; do
+            sleep 1
+            if [ "$current_song" != "$(playerctl -p $PLAYER metadata title)" ]; then
+                current_song=$(playerctl -p $PLAYER metadata title)
+                image_url=$(playerctl -p $PLAYER metadata mpris:artUrl)
+                curl $image_url 2> /dev/null > $temp_dir/pic.jpg
+                echo "$temp_dir/pic.jpg"
+            fi
+        done
+        ;;
 esac
