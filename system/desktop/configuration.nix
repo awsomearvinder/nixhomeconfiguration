15bb65c2c60eab16a9390d9edd17c83bec87ae11
Arvinder <ArvinderDhan@gmail.com>
2022-10-15 03:21:02 -0500

    feat: enable folding@home again.

--- system/desktop/configuration.nix
+++ system/desktop/configuration.nix
@@ -156,6 +156,8 @@
 
   environment.variables.EDITOR = "nvim";
 
+  services.foldingathome.enable = true;
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   programs.mtr.enable = true;
