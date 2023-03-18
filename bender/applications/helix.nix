dbaafa48e60ca9394f9e9bf6b0e055b0929faf08
Arvinder <ArvinderDhan@gmail.com>
2023-03-18 15:24:13 -0500

    feat: impl helix formatting for nix

--- bender/applications/helix.nix
+++ bender/applications/helix.nix
@@ -1,5 +1,19 @@
-{ ... }: {
+{ pkgs, ... }: {
+  home.packages = [
+    pkgs.alejandra
+  ];
   programs.helix = {
     enable = true;
+    languages = [{
+      name = "nix";
+      auto-format = true;
+      config = {
+        nil = {
+          formatting = {
+            command = ["alejandra"  "--quiet"];
+          };
+        };
+      };
+    }];
   };
 }
