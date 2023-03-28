917b62fed4e68905e517cfe35ae949a5f46105e1
Arvinder <ArvinderDhan@gmail.com>
2023-03-28 09:37:49 -0500

    fix: eww: don't specify player

--- bender/dotfiles/eww_bar/scripts/music.sh
+++ bender/dotfiles/eww_bar/scripts/music.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
-PLAYER="tidal-hifi"
 case $1 in
     "current-song")
         while true; do
             sleep 0.05
-            playerctl -p $PLAYER metadata title
+            playerctl metadata title
         done
         ;;
     "image")
@@ -12,9 +11,9 @@ case $1 in
         current_song=""
         while true; do
             sleep 0.05
-            if [ "$current_song" != "$(playerctl -p $PLAYER metadata title)" ]; then
-                current_song=$(playerctl -p $PLAYER metadata title)
-                image_url=$(playerctl -p $PLAYER metadata mpris:artUrl)
+            if [ "$current_song" != "$(playerctl metadata title)" ]; then
+                current_song=$(playerctl metadata title)
+                image_url=$(playerctl metadata mpris:artUrl)
                 curl $image_url 2> /dev/null > $temp_dir/pic.jpg
                 echo "$temp_dir/pic.jpg"
             fi
