c397201897ff06174f66e4ac1d459b4a21c47263
Arvinder <arvinder.dhanoa@winona.edu>
2023-07-28 14:30:24 -0500

    feat(git): add support for sapling
    large diff bc formatter, oops.

--- bender/applications/git.nix
+++ bender/applications/git.nix
@@ -2,39 +2,50 @@
   config,
   pkgs,
   ...
-}: {
+}: let
+  name = "Arvinder";
+  email =
+    if config.work_account
+    then "arvinder.dhanoa@winona.edu"
+    else "ArvinderDhan@gmail.com";
+  gpg_key =
+    if config.work_account
+    then "D938E040245154F8"
+    else "687341EC5B73B98BC5E4DC5D4599D30196519D5F";
+in {
   home.packages = [
     pkgs.sapling
   ];
 
-  programs.git =
-    {
-      enable = true;
-      userName = "Arvinder";
-      userEmail = "ArvinderDhan@gmail.com";
-      difftastic.enable = true;
-      difftastic.background = "dark";
-      extraConfig =
-        {
-          pull.ff = "only";
-          user.signingkey = "687341EC5B73B98BC5E4DC5D4599D30196519D5F";
-          commit.gpgSign = true;
-          gpg.program = "${pkgs.gnupg}/bin/gpg";
-          rerere.enabled = true;
-        }
-        // (
-          if config.work_account
-          then {user.signingkey = "D938E040245154F8";}
-          else {}
-        );
-    }
-    // (
-      if config.work_account
-      then {
-        userEmail = "Arvinder.Dhanoa@winona.edu";
-      }
-      else {}
-    );
+  programs.gh = {
+    enable = true;
+    settings.git_protocol = "ssh";
+  };
+
+  xdg.configFile."sapling/sapling.conf".source = (pkgs.formats.ini {}).generate "sapling.conf" {
+    ui = {
+      username = "${name} <${email}>";
+      editor = "hx";
+    };
+    gpg = {
+      key = gpg_key;
+    };
+  };
+
+  programs.git = {
+    enable = true;
+    userName = name;
+    userEmail = email;
+    difftastic.enable = true;
+    difftastic.background = "dark";
+    extraConfig = {
+      pull.ff = "only";
+      user.signingkey = gpg_key;
+      commit.gpgSign = true;
+      gpg.program = "${pkgs.gnupg}/bin/gpg";
+      rerere.enabled = true;
+    };
+  };
   programs.lazygit.enable = true;
   programs.lazygit.settings = {
     git = {
