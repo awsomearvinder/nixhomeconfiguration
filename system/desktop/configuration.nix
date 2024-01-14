56159aa82f76512df84f04b6c8020733d2febd84
Arvinder Dhanoa <ArvinderDhan@gmail.com>
2024-01-14 14:48:47 -0600

    chore(flake): update flake.lock

--- system/desktop/configuration.nix
+++ system/desktop/configuration.nix
@@ -151,7 +151,6 @@
 
   environment.variables.EDITOR = "hx";
 
-  services.foldingathome.enable = true;
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
