9855662b10f0ae973dce27c055c4879dfff892e3
Arvinder Dhanoa <ArvinderDhan@gmail.com>
2024-11-23 17:53:56 -0600

    feat(applications/eww): drop hacks for music dropdown w/ `tidal-hifi`
    It now works for generic music clients. This is fine since I moved towards
    a custom jellyfin setup on my home-server, and now use Feishin.

--- bender/dotfiles/eww_bar/scripts/music.sh
+++ bender/dotfiles/eww_bar/scripts/music.sh
@@ -5,12 +5,14 @@ case $1 in
         ;;
 
     "progress")
-        playerctl metadata --format "{{ position * 100 / mpris:length }}" --follow
+        while true; do
+            playerctl metadata --format "{{ position * 100 / mpris:length }}"
+        done
         ;;
 
     "set-progress")
-        offset=$(playerctl -p tidal-hifi metadata --format "{{ ($2 * mpris:length) / 100000000 }}")
-        playerctl -p tidal-hifi position "$offset" +
+        offset=$(playerctl metadata --format "{{ ($2 * mpris:length) / 100000000 }}")
+        playerctl position "$offset" +
         # fi
         ;;
 
@@ -19,7 +21,7 @@ case $1 in
         image_folder=${XDG_RUNTIME_DIR:-${TMPDIR:-${TMP:-${TEMP:-/tmp}}}}/artUrls
         mkdir -p $image_folder 2> /dev/null
 
-        playerctl -p tidal-hifi --follow metadata mpris:artUrl | while read url; do
+        playerctl --follow metadata mpris:artUrl | while read url; do
           file_name=$(echo "$url" | sed -s 's/\//_/g')
           if [ ! -f "$image_folder/$file_name" ]; then
               curl "$url" > "$image_folder/$file_name" 2> /dev/null
