55a25e0ba25f737bf192fd4213dca560d10d93de
Arvinder Dhanoa <ArvinderDhan@gmail.com>
2025-01-15 10:37:11 -0600

    feat(laptop/configuration.nix): enable bluetooth

--- system/laptop/configuration.nix
+++ system/laptop/configuration.nix
@@ -68,6 +68,11 @@
     #media-session.enable = true;
   };
 
+  hardware.bluetooth = {
+    enable = true;
+    powerOnBoot = true;
+  };
+
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
 
