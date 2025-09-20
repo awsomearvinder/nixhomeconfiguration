00a0061743396ed4d8e4ed30594c55c6f6b2d0b3
Arvinder Dhanoa <git@mail.arvinderd.com>
2025-09-19 21:14:32 -0500

    feat(system/desktop/hardware-configuration): remove swapDevice
    the swap device was mounted on my hard drive I gave to my friend
    so he could backup his disk (which was on his failing laptop...)
    
    :)

--- system/desktop/hardware-configuration.nix
+++ system/desktop/hardware-configuration.nix
@@ -29,11 +29,6 @@
       options = [ "fmask=0022" "dmask=0022" ];
     };
 
-
-  swapDevices =
-    [ { device = "/dev/disk/by-uuid/701e9251-bf6d-41f8-8118-93cafc5e7772"; }
-    ];
-
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
